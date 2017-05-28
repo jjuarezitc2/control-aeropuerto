1bf1655a547fc7368f8ff97f736830ea04d0fea3
jjuarezitc2 <mbotchitc@gmail.com>
2017-05-28 16:56:04 -0500

    Añadida la clase Pista

--- Aeropuerto/src/pistas/Pista.java
+++ Aeropuerto/src/pistas/Pista.java
@@ -1,5 +1,38 @@
 package pistas;
 
-public class Pista {
+import aeropuertos.*;
 
+/** 
+ * 
+ */
+public class Pista {
+	
+	/*
+	 * Identificador único para una pista en específico.
+	 * ejemplo 'P1'. Este valor debería ser irrepetible
+	 * en las pistas contenidas dentro de un aeropuerto
+	 * en específico. Es decir, solo habrá una pista 'P1'
+	 * en el aeropuerto Mariano Escobedo.
+	 */
+	private String codigo;
+	private Aeropuerto aeropuerto;
+	
+	public Pista(String codigo, Aeropuerto aeropuerto){
+		this.codigo = codigo;
+		this.aeropuerto = aeropuerto;
+	}
+	public String getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+	public Aeropuerto getAeropuerto() {
+		return aeropuerto;
+	}
+	public void setAeropuerto(Aeropuerto aeropuerto) {
+		this.aeropuerto = aeropuerto;
+	}
+	
+	
 }

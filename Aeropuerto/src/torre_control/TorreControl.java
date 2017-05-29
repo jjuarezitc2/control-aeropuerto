611de330010d785af0a76b2ad87e4ad3fb63daec
jjuarezitc2 <mbotchitc@gmail.com>
2017-05-29 13:38:46 -0500

    Añadida nueva funcionalidad login

---
+++ Aeropuerto/src/torre_control/TorreControl.java
@@ -0,0 +1,25 @@
+package torre_control;
+
+import aeropuertos.Aeropuerto;
+
+public class TorreControl {
+	
+	private int id;
+	private Aeropuerto aeropuerto;
+	
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public Aeropuerto getAeropuerto() {
+		return aeropuerto;
+	}
+	public void setAeropuerto(Aeropuerto aeropuerto) {
+		this.aeropuerto = aeropuerto;
+	}
+	
+	
+}

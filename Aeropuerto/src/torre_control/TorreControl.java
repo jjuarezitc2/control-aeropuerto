667ddcabeb0839ea174ac960ecfcf36b0f5cf5c4
jjuarezitc2 <mbotchitc@gmail.com>
2017-05-29 14:09:13 -0500

    Añadida clase torre de control

--- Aeropuerto/src/torre_control/TorreControl.java
+++ Aeropuerto/src/torre_control/TorreControl.java
@@ -1,25 +1,6 @@
 package torre_control;
 
-import aeropuertos.Aeropuerto;
-
 public class TorreControl {
-	
-	private int id;
-	private Aeropuerto aeropuerto;
-	
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public Aeropuerto getAeropuerto() {
-		return aeropuerto;
-	}
-	public void setAeropuerto(Aeropuerto aeropuerto) {
-		this.aeropuerto = aeropuerto;
-	}
-	
+
 	
 }
